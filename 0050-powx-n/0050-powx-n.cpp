4d29c112512cd272756f8a58f8de8d30f715660d
Vaibhav Kumar Singh <vaibhavsingh46614@gmail.com>
2026-08-24 16:59:24 +0530

    Time: 0 ms (100%), Space: 8.5 MB (93.34%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1.0;
+        long pow = n;
+
+        if(pow < 0){
+            pow = -1 * pow;
+        }
+
+        while( pow > 0){
+            if(pow % 2 == 1){
+                ans = ans * x;
+                pow = pow - 1;
+            }
+            else{
+                x = x * x;
+                pow = pow / 2;
+            }
+        }
+        if(n<0) ans = (double)(1.0) / (double)(ans);
+        return ans;
+    }
+};
